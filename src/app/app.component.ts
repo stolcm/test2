c5402b4268d5ada50858af400944210815769f19
unknown <stolcml@gmail.com>
2022-10-24 20:16:23 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'test';
+  
+  constructor(private http: HttpClient){}
+
+posts: any[] = []
+loadPosts(){
+this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((posts: any)=>
+{
+this.posts = posts;
+console.log(posts)
+})};
+
+unloadPosts(){
+window.location.reload()
+}
+
 }
