f23ea5361468756b6ff4dabc254a2274ec9b8647
unknown <stolcml@gmail.com>
2022-10-30 02:40:58 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,9 @@ export class AppComponent {
   
   constructor(private http: HttpClient){}
 
+ngAfterContentInit(){
+this.loadPosts()}
+
 posts: any[] = []
 loadPosts(){
 this.http.get('http://localhost:8000/get_post_endpoint/').subscribe((posts: any)=>
@@ -20,6 +23,23 @@ this.http.get('http://localhost:8000/get_post_endpoint/').subscribe((posts: any)
 this.posts = posts;
 console.log(posts)
 })};
+
+deletePost(id:any){
+this.http.delete('http://localhost:8000/delete_endpoint/'+id).subscribe(
+(data) => {
+        this.loadPosts();})
+
+}
+
+postPost(){
+this.http.post('http://localhost:8000/get_post_endpoint/',{
+        "id": "",
+        "title": "Title",
+        "description": "Description...",
+        "completed": "Not Completed"
+    }).subscribe((data) => {
+        this.loadPosts();})
+}
 
 unloadPosts(){
 window.location.reload()
